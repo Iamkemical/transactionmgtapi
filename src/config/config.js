2883177af97e13eca4ffb1182cddae2511554647
Gabriel Isaac <iamkemical1@gmail.com>
2024-10-19 23:34:14 +0100

    Updated config.js to allow changes to be deployed to Render

--- src/config/config.js
+++ src/config/config.js
@@ -1,27 +1,31 @@
-require('dotenv').config({path: `${process.cwd()}/.env`});
+require("dotenv").config({ path: `${process.cwd()}/.env` });
 
 module.exports = {
-  "development": {
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DB,
-    "host": process.env.POSTGRES_HOST,
-    "port": process.env.POSTGRES_PORT,
-    "dialect": "postgres",
-    "seederStorage": 'sequelize'
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true, // Enable SSL
+        rejectUnauthorized: false, // Disable SSL certificate validation (use with caution)
+      },
+    },
+    seederStorage: "sequelize",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
