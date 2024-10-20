00cd20a465f4d7906dbed60d1759865a62db2b1e
Gabriel Isaac <iamkemical1@gmail.com>
2024-10-20 15:10:45 +0100

    Fixed bug with config and database setup

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: `${process.cwd()}/.env` });
 
 module.exports = {
   development: {
--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const { Sequelize, Op, QueryTypes } = require("sequelize");
 const env = process.env.NODE_ENV || "development";
 const config = require("./config");
 
-const sequelize = new Sequelize(config[env]);
+const sequelize = new Sequelize(process.env.DATABASE_URL, config[env]);
 
 async function testConnection() {
   try {
